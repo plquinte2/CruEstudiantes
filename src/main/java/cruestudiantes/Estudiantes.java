0eb41447ca0f2848bea5a60fb6abef8a38c6f84d
plquinte2 <plquinte2@gmail.com>
2022-09-06 21:03:52 -0500

    Primer comentario

---
+++ src/main/java/cruestudiantes/Estudiantes.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cruestudiantes;
+
+/**
+ *
+ * @author Luis Quintero
+ */
+public class Estudiantes {
+    public class Estudiante {
+        private int codigo;
+        private string nombre;
+        private double nota1;
+        private double nota2;
+        private double nota3;
+    }
+    
+}
---
+++ src/main/java/cruestudiantes/Menu.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cruestudiantes;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Luis Quintero
+ */
+public class Menu {
+    public static void opciones (){
+        Scanner leer= new Scanner(System.in);
+        int idmenu=0;
+        boolean salir=false;
+        
+        String texMenu="Menu de opciones 92n";
+        String [] listaMenu=("Crear estudiante" + " ");
+        
+        System.out.println("textoMenu");
+        System.out.println("Seleccionar una opcion: ");
+                
+    } 
+}
